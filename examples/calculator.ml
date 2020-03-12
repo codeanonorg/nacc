35d59744be2e6d748ec585bb803bbf1bb32582b9
Nathan Graule <solarliner@gmail.com>
2020-03-12 14:27:18 +0100

    Reformat

--- examples/calculator.ml
+++ examples/calculator.ml
@@ -10,37 +10,19 @@ type expr =
   | Pow of expr * expr
 [@@deriving variants, show]
 
-let rec parse_exp inp = inp --> begin
-    binop add '+' ~~parse_mul
-    <|>
-    binop sub '-' ~~parse_mul
-    <|>
-    ~~parse_mul
-  end
+let rec parse_exp inp =
+  inp
+  --> (binop add '+' ~~parse_mul <|> binop sub '-' ~~parse_mul <|> ~~parse_mul)
 
-and parse_mul inp = inp --> begin
-    binop mul '*' ~~parse_pow
-    <|>
-    binop div '/' ~~parse_pow
-    <|>
-    ~~parse_pow
-  end
+and parse_mul inp =
+  inp
+  --> (binop mul '*' ~~parse_pow <|> binop div '/' ~~parse_pow <|> ~~parse_pow)
 
-and parse_pow inp = inp --> begin
-    binop pow '^' ~~parse_fac
-    <|>
-    ~~parse_fac
-  end
+and parse_pow inp = inp --> (binop pow '^' ~~parse_fac <|> ~~parse_fac)
 
-and parse_fac inp = inp --> begin
-    parenthesized '(' ~~parse_exp ')'
-    <|>
-    ~~parse_cst
-  end
+and parse_fac inp = inp --> (parenthesized '(' ~~parse_exp ')' <|> ~~parse_cst)
 
-and parse_cst inp = inp --> begin
-    cst <$> floatingpoint
-  end
+and parse_cst inp = inp --> (cst <$> floatingpoint)
 
 (* let _ =
   do_parse ~~parse_exp "1+2*3"
@@ -63,24 +45,25 @@ let _ =
   |> Option.get |> show_expr |> print_endline
  *)
 
-let rec eval =
-  function
+let rec eval = function
   | Cst v -> v
-  | Add(l,r) -> (eval l) +. (eval r)
-  | Sub(l,r) -> (eval l) -. (eval r)
-  | Mul(l,r) -> (eval l) *. (eval r)
-  | Div(l,r) -> (eval l) /. (eval r)
-  | Pow(l,r) -> Float.pow (eval l) (eval r)
+  | Add (l, r) -> eval l +. eval r
+  | Sub (l, r) -> eval l -. eval r
+  | Mul (l, r) -> eval l *. eval r
+  | Div (l, r) -> eval l /. eval r
+  | Pow (l, r) -> Float.pow (eval l) (eval r)
 
 let _ =
   while true do
     print_string "Calc # ";
     flush stdout;
-    let fst (a,_) = a in
-    try read_line() --> ~~parse_exp |> Option.get |> fst |> eval |> string_of_float |> print_endline
+    let fst (a, _) = a in
+    try
+      read_line () --> ~~parse_exp
+      |> Option.get |> fst |> eval |> string_of_float |> print_endline
     with
     | Invalid_argument _ -> print_endline "Syntax error"
     | End_of_file ->
-      print_endline "Bye.";
-      exit 0
+        print_endline "Bye.";
+        exit 0
   done
--- lib/parsing.mli
+++ lib/parsing.mli
@@ -11,16 +11,28 @@
 
 type 'a parser
 
-val pure      : 'a -> 'a parser
-val (<$>)     : ('a -> 'b) -> 'a parser -> 'b parser
-val (<*>)     : ('a -> 'b) parser -> 'a parser -> 'b parser
-val ( <* )    : 'a parser -> 'b parser -> 'a parser
-val ( *> )    : 'a parser -> 'b parser -> 'b parser
-val (<|>)     : 'a parser -> 'a parser -> 'a parser
-val do_parse  : 'a parser -> string -> 'a option
-val (-->)     : string -> 'a parser -> ('a * string) option
-val (<--)     : 'a parser -> string -> ('a * string) option
-val many      : 'a parser -> 'a list parser
-val some      : 'a parser -> 'a list parser
-val check     : (char -> bool) -> char parser
-val (~~)      : (string -> ('a * string) option) -> 'a parser
+val pure : 'a -> 'a parser
+
+val ( <$> ) : ('a -> 'b) -> 'a parser -> 'b parser
+
+val ( <*> ) : ('a -> 'b) parser -> 'a parser -> 'b parser
+
+val ( <* ) : 'a parser -> 'b parser -> 'a parser
+
+val ( *> ) : 'a parser -> 'b parser -> 'b parser
+
+val ( <|> ) : 'a parser -> 'a parser -> 'a parser
+
+val do_parse : 'a parser -> string -> 'a option
+
+val ( --> ) : string -> 'a parser -> ('a * string) option
+
+val ( <-- ) : 'a parser -> string -> ('a * string) option
+
+val many : 'a parser -> 'a list parser
+
+val some : 'a parser -> 'a list parser
+
+val check : (char -> bool) -> char parser
+
+val ( ~~ ) : (string -> ('a * string) option) -> 'a parser
