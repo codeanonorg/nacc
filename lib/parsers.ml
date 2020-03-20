95e6da6c07b64bea0e8447a17372741ecc88fcaa
Nathan Graule <solarliner@gmail.com>
2020-03-20 17:44:37 +0100

    Fix floating point parser

--- lib/parsers.ml
+++ lib/parsers.ml
@@ -32,22 +32,29 @@ let integer =
   in
   convert <$> some (one_in "0123456789")
 
-let floatingpoint =
-  let convert l =
+(* let floatingpoint =
+   let convert l =
     List.(fold_left ( ^ ) "" (map (String.make 1) l)) |> float_of_string
-  in
-  let concat a b = List.concat [ a; b ] in
-  let ( &> ) p1 p2 = concat <$> p1 <*> p2 in
-  let maybe p inp =
+   in
+   let concat a b = List.concat [ a; b ] in
+   let ( &> ) p1 p2 = concat <$> p1 <*> p2 in
+   let maybe p inp =
     match inp --> p with
     | (Some v, o, rest) -> (Some [v], o, rest)
     | (None, o, rest) -> (Some [], o, rest)
-  in
-  convert
-  <$> ( many (one_in "0123456789")
+   in
+   convert
+   <$> ( many (one_in "0123456789")
         &> ~~(maybe (char '.'))
         &> some (one_in "0123456789")
-        <|> many (one_in "0123456789") )
+        <|> many (one_in "0123456789") ) *)
+
+let floatingpoint =
+  let join l = List.(fold_left (^) "" (map (String.make 1) l)) in
+  let convert i f = float_of_int i +. float_of_string ("." ^ f) in
+  convert 0 <$> (char '.') *> (join <$> many (one_in "0123456789"))
+  <|> (convert <$> integer <*> char '.' *> (join <$> some (one_in "0123456789")))
+  <|> (float_of_int <$> integer)
 
 (** Parser for binary operations patterns
     @param  cons    a 2-parameters constructor
